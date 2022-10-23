d7374d7e62f876fff239d8714ed6e2434197ca15
Yungmaniie <olayeyeayomide2000@gmail.com>
2022-10-23 06:55:45 +0100

    data_types

---
+++ data_types.php
@@ -0,0 +1,31 @@
+/*integer*/
+
+echo "integer values <br>";
+$x =7;
+$y = -8;
+
+//to know the data_type we can use
+
+var_dump($x);
+var_dump($y);
+
+echo "<br>";
+echo "float values <br>";
+
+//float
+
+$float1 = 7.5;
+$float2 = -8.5;
+
+var_dump(float1);
+var_dump(float2);
+
+//string
+
+$string1 ="Hello world!";
+$string2 = 'Hello Muideen';
+
+var_dump($string1); 
+
+
+$bool = true;
