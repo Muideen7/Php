fa2c183714c1406cb27b10d95876a19a2a1765e9
Yungmaniie <olayeyeayomide2000@gmail.com>
2022-10-23 07:36:14 +0100

    arithmetics

--- operators/arithmetic.php
+++ operators/arithmetic.php
@@ -1,4 +1,20 @@
 <?php
 
-first = 18;
+$num1 = 18;
+$num2 = 7;
+
+echo $num1 + $num2;
+echo "<br>";
+echo $num1 - $num2;
+echo "<br>";
+echo $num1 * $num2;
+echo "<br>";
+echo $num1 / $num2;
+echo "<br>";
+echo $num1 % $num2;
+
+
+
+
+?>
 
